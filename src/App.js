adcf20c260a7aaf125b90015a6b6688c8f62a7b8
weather98 <112868136+weather98@users.noreply.github.com>
2022-09-27 10:35:32 +0900

    react hook1
    class에서 state 사용법

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import AddNumberRoot from './components/AddNumberRoot';
-import DisplayNumberRoot from './components/DisplayNumberRoot';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
+    <div className="container">
+      <h1>Hello world!2!</h1>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
+function FuncComp(프롭) {
+  return (
+    <div className="container">
+      <h2>function style Component</h2>
+      <p>Number : {프롭.initNumber}</p>
+    </div>
+  );
+}
+class ClassComp extends React.Component {
+  state = {
+    number:this.props.initNumber
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+      </div>
+    )
+  }
+}
 export default App;
--- src/components/app.js
+++
@@ -1,9 +0,0 @@
-// components/App.js
-const App = () => {
-    return <Button text="IT IS BUTTON"></Button>;
-};
- 
-// components/Button/index.js
-const Button = (props) => {
-  return <button>{props.text}</button>;
-};
