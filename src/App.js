5a9d7873422f97d9ec74e586c8bd8f033e856062
weather98 <112868136+weather98@users.noreply.github.com>
2022-11-08 18:12:13 +0900

    ajax2222

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class NNav extends Component {
-  state = {
-    list:[]
-  }
-  componentDidMount(){
-    fetch('list.json')
-      .then(function(result){
-        return result.json();
-      })
-      .then(function(json){
-        console.log(json);
-        this.setState({list:json});
-      }.bind(this));
-  }
   render() {
     var listTag = [];
-    for(var i=0; i<this.state.list.length; i++){
-      var li = this.state.list[i];
-      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+    for (var i = 0; i < this.props.list.length; i++) {
+      var li = this.props.list[i];
+      listTag.push(
+        <li key={li.id}>
+          <a href={li.id} data-id={li.id} onClick={function (e) {
+            e.preventDefault();
+            console.log('trigger');
+            this.props.onClick(e.target.dataset.id)
+          }.bind(this)}>
+            {li.title}
+          </a>
+        </li>
+      )
     }
     return (
       <nav>
@@ -30,17 +27,56 @@ class NNav extends Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <NNav></NNav>
+class Article extends Component {
+  render() {
+
+
+    return (
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
-      </article>
-    </div>
-  );
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article >
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    article: { title: 'Welcome', desc: 'Hello, React & Ajax' },
+    list:[]
+  }
+  componentDidMount() {
+    fetch('list.json')
+      .then(function (result) {
+        return result.json();
+      })
+      .then(function (json) {
+        console.log(json);
+        this.setState({ list: json });
+      }.bind(this));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <NNav list={this.state.list} onClick={function (id) {
+          fetch(id + '.json')
+            .then(function (result) {
+              return result.json();
+            })
+            .then(function (json) {
+              this.setState({
+                article:{
+                  title:json.title,
+                  desc:json.desc
+                }
+            })
+            }.bind(this))
+        }.bind(this)}></NNav>
+        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
